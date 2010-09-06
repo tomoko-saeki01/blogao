d224aa24cb7b2239ae8b5eb16f19772d24f81eaf
Catharine <Catharine@localhost>
2010-09-06 14:35:52 -0300

    Commit da classe Finder

---
+++ src/br/edu/ufcg/blogao/webservice/Finder.java
@@ -0,0 +1,27 @@
+package br.edu.ufcg.blogao.webservice;
+
+/** 
+ * 
+ * @author Caio
+ * @author Carlos
+ * @author Catharine
+ * @author Demontie
+ * @author Matheus
+ */
+public class Finder {
+
+	public static Object findContent(String soundId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Object findPost(String postId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static String findPostInfo(String postId, String atributo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
