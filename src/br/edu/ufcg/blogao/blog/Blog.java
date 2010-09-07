be1d2dd3ba9b394493c674be3a39df1837676e2d
Catharine <Catharine@localhost>
2010-09-07 18:46:26 -0300

    Java Doc da classe Blog. Criacao dos m?todos da classe Blog. Falta apenas pegar a data do sistema.

--- src/br/edu/ufcg/blogao/blog/Blog.java
+++ src/br/edu/ufcg/blogao/blog/Blog.java
@@ -1,76 +1,153 @@
 package br.edu.ufcg.blogao.blog;
 
+/**
+ * Represents a blog.
+ * @author Caio
+ * @author Carlos
+ * @author Catharine
+ * @author Demontie
+ * @author Matheus
+ * @version 1.0 - 7th September, 2010.
+ */
+
+import java.util.Collection;
 import java.util.Date;
-import java.util.List;
+import java.util.Map;
 
 import br.edu.ufcg.blogao.blog.data.StaticContent;
 
 public class Blog implements WebElement {
-	
-	private String id;
+	//Blog "default" attributes.
 	private String title;
+	
+	//
+	private String id;
 	private Date date;
 	private StaticContent description;
-	private List<Blog> subBlogs;
-	private List<Post> posts;
+	private Map<String, Blog> subBlogs; //<id, blog>
+	private Map<String, Post> posts; //<id, post>
 	
-	@Override
-	public Date getCreationDate() {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * Default constructor.
+	 * @param title The blog's title.
+	 */
+	public Blog(String title) {
+		this(title, null);
 	}
 	
+	/**
+	 * Another constructor.
+	 * @param title The blog's title.
+	 * @param description The blog's description.
+	 */
+	public Blog(String title, StaticContent description) {
+		this.title = title;
+		this.description = description;
+		
+		//TODO Creation date.
+	}
+	
+	/**
+	 * Add a post to the blog.
+	 * @param id The post's ID.
+	 * @param post The post.
+	 */
+	public void addPost(String id, Post post) {
+		posts.put(id, post);
+	}
+	
+	/**
+	 * Return blog's date creation.
+	 */
+	@Override
+	public Date getCreationDate() {
+		return date;
+	}
+	
+	/**
+	 * Return the blog's ID.
+	 */
 	@Override
 	public String getId() {
 		return id;
 	}
 	
+	/**
+	 * Return the total number of posts.
+	 * @return The total number of posts.
+	 */
+	public Integer getNumberOfPosts() {
+		return posts.size();
+	}
+	
+	/**
+	 * Return the total number of subBlogs.
+	 * @return The total number of subBlogs.
+	 */
+	public Integer getNumberOfSubBlogs() {
+		return subBlogs.size();
+	}
+	
+	/**
+	 * Return the total number of all subBlogs.
+	 * @return The total number of all subBlogs.
+	 */
+	public Integer getNumberOfAllSubBlogs() {
+		int total = 0;
+		Collection<Blog> sub = subBlogs.values();
+		for (Blog b: sub) {
+			total += b.getNumberOfSubBlogs();
+		}		
+		return total;
+	}
+	
+	/**
+	 * Return a blog's subBlog.
+	 * @param index The subBlog's ID.
+	 * @return A blog's subBlog.
+	 */
+	public Blog getSubBlog(int index) {
+		return subBlogs.get(index);
+	}
+	
+	/**
+	 * Return the blog's description. 
+	 */
 	@Override
 	public StaticContent getText() {
 		return description;
 	}
+	
+	/**
+	 * Return the blog's title.
+	 * @return The blog's title.
+	 */
+	public String getTitle() {
+		return title;
+	}
 
+	/**
+	 * Set the blog's description.
+	 */
 	@Override
 	public void setText(StaticContent text) {
 		this.description = text;
 	}
 	
-	public String getTitle() {
-		return title;
+	/**
+	 * Return a blog's post.
+	 * @param index The post's ID.
+	 * @return A blog's post.
+	 */
+	public Post getPost(int index) {
+		return posts.get(index);
 	}
 
+	/**
+	 * Set the blog's title.
+	 * @param title The blog's title.
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
-	public String createPost(String title, String text) {
-		// TODO
-		return null;
-	}
-	
-	public Post getPost(int index) {
-		// TODO
-		return null;
-	}
-	
-	public Integer getNumberOfPosts() {
-		// TODO
-		return null;
-	}
-	
-	public Blog getSubBlog(int index) {
-		// TODO
-		return null;
-	}
-	
-	public Integer getNumberOfSubBlogs() {
-		// TODO
-		return null;
-	}
-	
-	public Integer getNumberOfAllSubBlogs() {
-		// TODO
-		return null;
-	}
-
 }
