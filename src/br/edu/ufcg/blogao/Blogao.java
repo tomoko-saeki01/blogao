4fc608cb40b10098c1b29091930d3109143387e8
caiopaes <caiopaes@localhost>
2010-09-25 19:20:04 -0300

    --- Algumas mudan?as no build.
    --- Agora o .id fica em resources/db/
    
    --- Fiz um mudan?a que se rodar o Blogao sem existir o .id ele ser? criado.
    
    --- Adaptei a Fachada do WS de acordo com o que William prop?s no grupo e que Carolina acatou.

--- src/br/edu/ufcg/blogao/Blogao.java
+++ src/br/edu/ufcg/blogao/Blogao.java
@@ -5,6 +5,9 @@ import br.edu.ufcg.blogao.webservice.HelperServer;
 public class Blogao {
 
 	/**
+	 * Starts Blogao application starting web service.
+	 * 
+	 * The WSDL is provided at <a href="http://localhost:8080/blogao?wsdl">http://localhost:8080/blogao?wsdl</a>
 	 * @param args
 	 */
 	public static void main(String[] args) {
