77148b6eecc23da05f53f855f8283637a4147742
Catharine <Catharine@localhost>
2010-11-19 00:57:31 -0300

    Mais implementa????o grafica.

--- src/br/edu/ufcg/blogao/GUI/RegisterUserFrame.java
+++ src/br/edu/ufcg/blogao/GUI/RegisterUserFrame.java
@@ -1,5 +1,6 @@
 package br.edu.ufcg.blogao.GUI;
 
+import com.trolltech.qt.core.QDate;
 import com.trolltech.qt.gui.QComboBox;
 import com.trolltech.qt.gui.QFont;
 import com.trolltech.qt.gui.QLabel;
@@ -14,10 +15,10 @@ public class RegisterUserFrame extends QWidget {
 			emailLabel, interestsLabel, whoIAmLabel, dateBirthdayLabel,
 			sexLabel, musicsLabel, moviesLabel, booksLabel;
 	private QLineEdit loginField, passwordField, nameField, addressField,
-			emailField, interestsField, whoIAmField, dateBirthdayField,
-			musicsField, moviesField, booksField;
-	private QComboBox comboMonth;
-	private QLabel month;
+			emailField, interestsField, whoIAmField, musicsField, moviesField,
+			booksField;
+	private QComboBox comboMonth, comboDay, comboYear;
+	private QLabel month, sex;
 
 	public RegisterUserFrame() {
 		setWindowTitle("Blogão");
@@ -49,9 +50,27 @@ public class RegisterUserFrame extends QWidget {
 		 */
 	}
 
-	private void createComoMonth() {
-		comboMonth = new QComboBox(this);
-		comboMonth.addItem("");
+	private void createComboDay() {
+		comboDay = createComboBox();
+		comboDay.addItem("Dia");
+		// TODO
+	}
+
+	private void createComboYear() {
+		comboYear = createComboBox();
+		comboYear.addItem("Ano");
+		QDate date = new QDate();
+
+		@SuppressWarnings("static-access")
+		int year = date.currentDate().year();
+		for (int i = 1960; i <= year; i++) {
+			comboYear.addItem("" + i);
+		}
+	}
+
+	private void createComboMonth() {
+		comboMonth = createComboBox();
+		comboMonth.addItem("Mês");
 		comboMonth.addItem("Janeiro");
 		comboMonth.addItem("Fevereiro");
 		comboMonth.addItem("Março");
@@ -68,109 +87,109 @@ public class RegisterUserFrame extends QWidget {
 
 	private void InitAddressFields() {
 		String addressText = "Endereço";
-		addressLabel = new QLabel(addressText, this);
+		addressLabel = createLabel(addressText);
 		addressLabel.setFont(new QFont("Tempus Sans ITC", 11));
 		addressLabel.move(300, 180);
 
-		addressField = new QLineEdit(this);
+		addressField = createLineEdit();
 		addressField.resize(250, 25);
 		addressField.move(460, 180);
 	}
 
 	private void InitBooksFields() {
 		String booksText = "Livros Favoritos";
-		booksLabel = new QLabel(booksText, this);
+		booksLabel = createLabel(booksText);
 		booksLabel.setFont(new QFont("Tempus Sans ITC", 11));
 		booksLabel.move(300, 390);
 
-		booksField = new QLineEdit(this);
+		booksField = createLineEdit();
 		booksField.resize(250, 25);
 		booksField.move(460, 420);
 	}
 
 	private void InitDateBirthdayFields() {
 		String dateBirthdayText = "Data de Aniversário";
-		dateBirthdayLabel = new QLabel(dateBirthdayText, this);
+		dateBirthdayLabel = createLabel(dateBirthdayText);
 		dateBirthdayLabel.setFont(new QFont("Tempus Sans ITC", 11));
 		dateBirthdayLabel.move(300, 300);
 
-		createComoMonth();
+		createComboDay();
+		createComboMonth();
+		createComboYear();
 
-		month = new QLabel();
-		// comboMonth.currentStringChanged.connect(this, "OnActivated(String)");
+		month = createLabel("");
+		comboMonth.currentStringChanged.connect(this,
+				"OnActivatedMonth(String)");
 
-		comboMonth.move(490, 300);
-		// month.move(520, 300);
-
-		dateBirthdayField = new QLineEdit(this);
-		dateBirthdayField.resize(25, 25);
-		dateBirthdayField.move(460, 300);
+		comboDay.move(460, 300);
+		comboMonth.move(505, 300);
+		comboYear.move(584, 300);
 	}
 
 	private void InitEmailsFields() {
 		String emailText = "E-mail";
-		emailLabel = new QLabel(emailText, this);
+		emailLabel = createLabel(emailText);
 		emailLabel.setFont(new QFont("Tempus Sans ITC", 11));
 		emailLabel.move(300, 210);
 
-		emailField = new QLineEdit(this);
+		emailField = createLineEdit();
 		emailField.resize(250, 25);
 		emailField.move(460, 210);
 	}
 
 	private void InitInteretsFields() {
 		String interestsText = "Interesses";
-		interestsLabel = new QLabel(interestsText, this);
+		interestsLabel = createLabel(interestsText);
 		interestsLabel.setFont(new QFont("Tempus Sans ITC", 11));
 		interestsLabel.move(300, 240);
 
-		interestsField = new QLineEdit(this);
+		interestsField = createLineEdit();
 		interestsField.resize(250, 25);
 		interestsField.move(460, 240);
 	}
 
 	private void InitMoviesFields() {
 		String moviesText = "Filmes Favoritos";
-		moviesLabel = new QLabel(moviesText, this);
+		moviesLabel = createLabel(moviesText);
 		moviesLabel.setFont(new QFont("Tempus Sans ITC", 11));
 		moviesLabel.move(300, 360);
 
-		moviesField = new QLineEdit(this);
+		moviesField = createLineEdit();
 		moviesField.resize(250, 25);
 		moviesField.move(460, 390);
 	}
 
 	private void InitMusicsFields() {
 		String musicsText = "Músicas Favoritas";
-		musicsLabel = new QLabel(musicsText, this);
+		musicsLabel = createLabel(musicsText);
 		musicsLabel.setFont(new QFont("Tempus Sans ITC", 11));
 		musicsLabel.move(300, 420);
 
-		musicsField = new QLineEdit(this);
+		musicsField = createLineEdit();
 		musicsField.resize(250, 25);
 		musicsField.move(460, 360);
 	}
 
 	private void InitNameFields() {
 		String nameText = "Nome";
-		nameLabel = new QLabel(nameText, this);
+		nameLabel = createLabel(nameText);
 		nameLabel.setFont(new QFont("Tempus Sans ITC", 11));
 		nameLabel.move(300, 150);
 
-		nameField = new QLineEdit(this);
+		nameField = createLineEdit();
 		nameField.resize(250, 25);
 		nameField.move(460, 150);
 	}
 
 	private void InitSexFields() {
 		String sexText = "Sexo";
-		sexLabel = new QLabel(sexText, this);
+		sexLabel = createLabel(sexText);
 		sexLabel.setFont(new QFont("Tempus Sans ITC", 11));
 		sexLabel.move(300, 330);
 
-		QComboBox combo = new QComboBox(this);
+		QComboBox combo = createComboBox();
 
-		combo.addItem(" ");
+		combo.addItem("Sexo");
 		combo.addItem("Feminino");
 		combo.addItem("Masculino");
 		combo.move(460, 330);
@@ -178,18 +197,35 @@ public class RegisterUserFrame extends QWidget {
 
 	private void InitWhoIAmFields() {
 		String whoIAmText = "Quem Sou Eu";
-		whoIAmLabel = new QLabel(whoIAmText, this);
+		whoIAmLabel = createLabel(whoIAmText);
 		whoIAmLabel.setFont(new QFont("Tempus Sans ITC", 11));
 		whoIAmLabel.move(300, 270);
 
-		whoIAmField = new QLineEdit(this);
+		whoIAmField = createLineEdit();
 		whoIAmField.resize(250, 25);
 		whoIAmField.move(460, 270);
 	}
 
-	private void OnActivated(String text) {
+	private QComboBox createComboBox() {
+		return new QComboBox(this);
+	}
+
+	private QLabel createLabel(String text) {
+		return new QLabel(text, this);
+	}
+
+	private QLineEdit createLineEdit() {
+		return new QLineEdit(this);
+	}
+
+	@SuppressWarnings("unused")
+	private void OnActivatedMonth(String text) {
 		month.setText(text);
-		month.adjustSize();
+	}
+
+	@SuppressWarnings("unused")
+	private void OnActivatedSex(String text) {
+		sex.setText(text);
 	}
 
 	@SuppressWarnings("unused")
