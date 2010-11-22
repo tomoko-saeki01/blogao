d8b1b5473394b43c8edf60c2238e159e35c4a9bc
Catharine <Catharine@localhost>
2010-11-22 18:44:06 -0300

    Commit por Demontie. E commit meu tamb?m

--- src/br/edu/ufcg/blogao/GUI/LoginFrame.java
+++ src/br/edu/ufcg/blogao/GUI/LoginFrame.java
@@ -20,35 +20,55 @@ public class LoginFrame extends QWidget {
 	}
 
 	private void initUI() {
+		
 		//Cria o botão para login
-		loginButton = new QPushButton("Login", this);
+		loginButton = createButton("Login", this);
 		loginButton.resize(100, 25);
 		loginButton.setCheckable(true);
 		loginButton.toggled.connect(this, "onToggled()");
 		loginButton.move(590, 400);
-		
+				
+		createLoginField();
+		createPasswordField();
+	}
+	
+	private void createLoginField() {
 		//Cria o texto "login" na tela.
 		String loginText = "Login";
-		loginLabel = new QLabel(loginText, this);
+		loginLabel = createLabel(loginText, this);
 		loginLabel.setFont(new QFont("Tempus Sans ITC", 11));
 		loginLabel.move(480, 320);
 		
 		//Cria o campo para que o login seja inserido.
-		loginField = new QLineEdit(this);
+		loginField = createLineEdit(this);
 		loginField.resize(250, 25);
 		loginField.move(520, 320);
-		
+	}
+	
+	private void createPasswordField() {
 		//Cria o texto "senha" na tela.
 		String passwordText = "Senha";
-		passwordLabel = new QLabel(passwordText, this); 
+		passwordLabel = createLabel(passwordText, this); 
 		passwordLabel.setFont(new QFont("Tempus Sans ITC", 11));
 		passwordLabel.move(480, 350);
 		
 		//Cria o campo para que a senha seja inserida.
-		passwordField = new QLineEdit(this);
+		passwordField = createLineEdit(this);
 		passwordField.setEchoMode(QLineEdit.EchoMode.Password);
 		passwordField.resize(250, 25);
 		passwordField.move(520, 350);
+	}
+	
+	private QPushButton createButton(String text, QWidget parent) {
+		return new QPushButton(text, parent);
+	}
+	
+	private QLabel createLabel(String text, QWidget parent) {
+		return new QLabel(text, parent);
+	}
+	
+	private QLineEdit createLineEdit(QWidget parent) {
+		return new QLineEdit(parent);
 	}
 	
 	@SuppressWarnings("unused")
