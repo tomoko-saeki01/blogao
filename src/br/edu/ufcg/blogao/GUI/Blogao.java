d8ec61443fcdaa2b7a74458fa587f4af7395b742
Catharine <Catharine@localhost>
2010-11-23 16:40:02 -0300

    Novas classes da GUI. As anteriores (que eram de testes) foram removidas

---
+++ src/br/edu/ufcg/blogao/GUI/Blogao.java
@@ -0,0 +1,10 @@
+package br.edu.ufcg.blogao.GUI;
+
+public class Blogao {
+	
+	public Blogao() {
+	
+	}
+	
+	//Se conecta com o webservice
+}
---
+++ src/br/edu/ufcg/blogao/GUI/FrameBlogao.java
@@ -0,0 +1,24 @@
+package br.edu.ufcg.blogao.GUI;
+import com.trolltech.qt.gui.QApplication;
+import com.trolltech.qt.gui.QWidget;
+
+public class FrameBlogao extends QWidget{
+	
+	public FrameBlogao() {
+		setWindowTitle("Blogão");
+		initUI();
+		show();
+	}
+	
+	private void initUI() {
+		resize(maximumSize());
+		
+	}
+
+	public static void main(String[] args) {
+		QApplication.initialize(args);
+		FrameBlogao blogao = new FrameBlogao();
+		blogao.	setVisible(true);
+		QApplication.exec();
+	}
+}
