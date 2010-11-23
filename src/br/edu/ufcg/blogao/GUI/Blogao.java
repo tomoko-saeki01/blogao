448cccdd2c188edac101b118949c30d86e182c6f
Catharine <Catharine@localhost>
2010-11-23 16:54:17 -0300

    blogao

--- src/br/edu/ufcg/blogao/GUI/Blogao.java
+++ src/br/edu/ufcg/blogao/GUI/Blogao.java
@@ -1,10 +1,42 @@
 package br.edu.ufcg.blogao.GUI;
 
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import br.edu.ufcg.blogao.webservice.BlogWSImpl;
+
 public class Blogao {
 	
-	public Blogao() {
+	private BlogWSImpl webService;
 	
+	public Blogao() {
+		webService = new BlogWSImpl();
 	}
 	
-	//Se conecta com o webservice
+	public static Blogao loadBlogao() {
+        try {
+            XMLDecoder decoder = new XMLDecoder(new FileInputStream("build.xml"));
+            Blogao gcp = (Blogao) decoder.readObject();
+            decoder.close();
+            return gcp;
+        } catch (FileNotFoundException ex) {
+            return new Blogao();
+        }
+    }
+	
+	public void store() {
+        try {
+            XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(
+                    new FileOutputStream("build.xml")));
+            encoder.writeObject(this);
+            encoder.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Could not write.");
+        }
+    }
 }
