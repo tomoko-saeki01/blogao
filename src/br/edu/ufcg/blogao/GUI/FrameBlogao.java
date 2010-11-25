b162d457a5e5e091ba7084c05f535270a1d56504
Catharine <Catharine@localhost>
2010-11-25 12:50:09 -0300

    frame

--- src/br/edu/ufcg/blogao/GUI/FrameBlogao.java
+++ src/br/edu/ufcg/blogao/GUI/FrameBlogao.java
@@ -1,45 +1,36 @@
 package br.edu.ufcg.blogao.GUI;
 
-import br.edu.ufcg.blogao.user.UserIF;
-
-import com.trolltech.qt.gui.QMessageBox;
+import com.trolltech.qt.gui.QApplication;
+import com.trolltech.qt.gui.QIcon;
 import com.trolltech.qt.gui.QWidget;
 
 public class FrameBlogao extends QWidget {
 
 	private Blogao blogao;
-	@SuppressWarnings("unused")
-	private UserIF userLoggedOn;
-
+	
 	public FrameBlogao() {
-
+		setWindowTitle("Blogão");
+		init();
+		objectsActions();
+		show();
 	}
 
-	public boolean authenticate(String login, String password) {
-		/*
-		 * UserImpl user = (UserImpl)blogao.getUser(login); if (user == null) {
-		 * return false; } if (user.getPassword().equals(password)) {
-		 * userLoggedOn = user; //TODO ABRIR A TELA INICIAL DO SISTEMA return
-		 * true; }
-		 */
-		return false;
+	private void objectsActions() {
+		openWindow(new FrameLogin());
 	}
 
-	public void displayMessage(String string) {
-		// TODO testar se ta certo!!
-		QMessageBox.critical(this, "Error", string);
+	private void openWindow(QWidget f) {		
 	}
 
-	public void openNewFrame(QWidget frame) {
-		
+	private void init() {
+		resize(maximumSize());
+		setWindowIcon(new QIcon("pictures/icon.png"));
+		blogao = new Blogao();
 	}
 
-	public void openWindow(QWidget frame) {
-		frame.setVisible(true);
-		// TODO terminar metodo.
-	}
-
-	public void registerUser() {
-		blogao.createUser();		
+	public static void main(String[] args) {
+		QApplication.initialize(args);
+		new FrameBlogao();
+		QApplication.exec();
 	}
 }
